933a954edfd76069eb87260e6e9f75f8f0edf882
mong <shdq2@naver.com>
2020-10-13 22:34:04 +0900

    move folder

--- src/store/modules/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import login from './login';
-import collection from './collection';
-import authority from './authority';
-import location from './location';
-export default combineReducers({
-  login,collection,authority,location
-});
